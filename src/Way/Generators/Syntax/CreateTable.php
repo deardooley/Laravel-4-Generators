0a1be173341f9b1d23fd681c91f97385842b7ba6
Rion Dooley <dooley@tacc.utexas.edu>
2015-02-22 14:00:20 -0600

    Added support for automatically creating column definitions in Migrations when only a foreign key definition is provided.

--- src/Way/Generators/Syntax/CreateTable.php
+++ src/Way/Generators/Syntax/CreateTable.php
@@ -14,6 +14,25 @@ class CreateTable extends Table {
     {
         $migrationData = ['method' => 'create', 'table' => $migrationData['table']];
 
+        // Let's add in the foreign key columns if they have not been
+        // explicitly added by the user.
+        $originalFields = array_slice($fields, 0);
+        foreach($originalFields as $field)
+        {
+          if ( $field['type'] === 'foreign' )
+          {
+              if (!$this->hasForeignKeyColumn($field['field'], $originalFields))
+              {
+                  $foreignKeyColumn = [
+                      'field' => $field['field'],
+                      'type' => 'integer',
+                      'decorators' => [ 'unsigned' ]];
+
+                  array_unshift($fields, $foreignKeyColumn);
+              }
+          }
+        }
+
         // All new tables should have an identifier
         // Let's add that for the user automatically
         array_unshift($fields, ['field' => 'id', 'type' => 'increments']);
@@ -21,7 +40,29 @@ class CreateTable extends Table {
         // We'll also add timestamps to new tables for convenience
         array_push($fields, ['field' => '', 'type' => 'timestamps']);
 
+
         return (new AddToTable($this->file, $this->compiler))->add($migrationData, $fields);
     }
 
-} 
+    /**
+     * Check for an existing column definition for an
+     * existing foreign key name
+     *
+     * @param $columnName
+     * @param $fields
+     * @return mixed
+     */
+    private function hasForeignKeyColumn($columnName, $fields)
+    {
+      foreach(array_slice($fields, 0) as $index=>$field)
+      {
+        if ( $field['type'] === 'integer' && $field['field'] === $columnName )
+        {
+            return true;
+        }
+      }
+
+      return false;
+  }
+
+}
